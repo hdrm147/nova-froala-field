b490d11711e8532c526953c0c192a94564f6e1d1
Haidar Mahmoud <hdrm147@gmail.com>
2019-05-23 00:20:59 +0300

    fixes

--- src/Handlers/DeleteAttachments.php
+++ src/Handlers/DeleteAttachments.php
@@ -34,11 +34,6 @@ class DeleteAttachments
      */
     public function __invoke(Request $request, $model)
     {
-        $path = parse_url($request->attachmentUrl, PHP_URL_PATH);
-        $path = explode("/",$path);
-        $path = storage_path(end($path));
-        \File::delete($path);
-        \Log::info($path);
         Attachment::where('attachable_type', get_class($model))
                 ->where('attachable_id', $model->getKey())
                 ->get()
